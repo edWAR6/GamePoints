a06dec1d067fb4e7835c6f8331af71968c91dbd6
Eduardo Oviedo Blanco <eduado.oviedo@gmail.com>
2013-04-08 17:06:36 -0600

    New express node server

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/css'));
+app.use(express.static(__dirname + '/img'));
+app.use(express.static(__dirname + '/js'));
+
+app.register('.html', require('jade'));
+
+
+app.get('/hello.txt', function(req, res){
+  res.send('Hello World');
+});
+
+// controllers
+// admin
+
+app.get('/admin', function(req, res) {
+	res.render('admin/index.html');
+});
+
+
+
+
+
+
+
+app.listen(8080);
+console.log('Listening on port 8080');
