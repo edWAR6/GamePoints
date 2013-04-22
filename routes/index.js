e389ca8e2c7deb909c1500e22e23f4c1ce5fca94
Eduardo Oviedo Blanco <eduardo.oviedo@gmail.com>
2013-04-21 23:42:01 -0600

    facebook login

--- routes/index.js
+++ routes/index.js
@@ -22,13 +22,20 @@ exports.whoweare = function(req, res){
 /*
  * GET admin howitworks.
  */
-// exports.howitworks = function(req, res){
-// 	res.render('admin/howitworks', { title: 'Game Points Administration' });
-// };
+exports.howitworks = function(req, res){
+	res.render('admin/howitworks', { title: 'Game Points Administration' });
+};
 
 /*
  * GET admin microprovider.
  */
-// exports.microprovider = function(req, res){
-// 	res.render('admin/microprovider', { title: 'Game Points Administration' });
-// };
+exports.microprovider = function(req, res){
+	res.render('admin/microprovider', { title: 'Game Points Administration' });
+};
+
+/*
+ * GET admin signup.
+ */
+exports.signup = function(req, res){
+	res.render('admin/signup', { title: 'Game Points Administration' });
+};
